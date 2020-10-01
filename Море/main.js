d3cfc7605d22ee3fffbbfa496273c8b23fc2aa35
andrey_hacker <46724375+andreypikalov13@users.noreply.github.com>
2020-10-01 16:39:03 +0300

    Add files via upload

---
+++ Море/main.js
@@ -0,0 +1,5 @@
+let header = document.querySelector('header');
+header.style.height = document.documentElement.clientHeight +'px';
+console.log(header.style.height);
+console.log(document.body.scrollHeight);
+console.log(document.documentElement.scrollHeight);
